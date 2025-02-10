44030ba13b8915f5a9f98bec897321a54aa7f684
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-10 14:59:40 +0100

    test variable inputs

---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+Descrizione
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+*/
+
+const user_travel_km = parseInt(prompt("Inserisci i km del tuo viaggio! (niente bugie)"));
+console.log(user_travel_km);
+const user_age = parseInt(prompt("Inserisci la tua età! (niente bugie)"));
+console.log(user_age);
+
+
+
